f05957d69ab8c2089eaa42cb84649bd70b43253d
Ruan Oliveira <ruansilvaolv@gmail.com>
2026-02-23 18:54:43 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+
+[TestClass]
+public sealed class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
